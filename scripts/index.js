55f2a610638980e85641eb087eabc978f57c7d43
lisa-wolfgang <43426138+lisa-wolfgang@users.noreply.github.com>
2022-07-01 02:20:07 -0500

    More accurate centering behavior

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 let bubbleTypeElement = document.getElementById("bubble-type");
 let bubbleElement = document.querySelector(".bubble");
+let bubbleContentElement = document.querySelector(".bubble-content");
 let bubbleType = bubbleTypeElement.value;
 
 bubbleElement.classList.add("no-transition");
@@ -9,6 +10,11 @@ bubbleElement.classList.remove("no-transition");
 
 bubbleTypeElement.addEventListener("change", () => {
   updateBubbleType();
+});
+
+bubbleElement.addEventListener("mousedown", (e) => {
+  if (e.target == bubbleElement) e.preventDefault(); // prevents unfocus on text when clicked
+  bubbleContentElement.focus();
 });
 
 function updateBubbleType() {
