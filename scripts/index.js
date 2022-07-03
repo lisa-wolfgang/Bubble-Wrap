ae9d0817dda48985679d24334cc589d5b84463d7
lisa-wolfgang <43426138+lisa-wolfgang@users.noreply.github.com>
2022-07-03 01:06:10 -0500

    Add MSYT export feature

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,30 @@
 let bubbleTypeElement = document.getElementById("bubble-type");
 let bubbleElement = document.querySelector(".bubble");
 let bubbleContentElement = document.querySelector(".bubble-content");
+let exportBtnElement = document.getElementById("export-btn");
+
 let bubbleType = bubbleTypeElement.value;
 
+// Sets the previously selected bubble type on page load
 bubbleElement.classList.add("no-transition");
 updateBubbleType();
 bubbleElement.parentElement.offsetHeight; // necessary to force CSS reload in time
 bubbleElement.classList.remove("no-transition");
 
+// Create test bubble
+let testBubbleText = document.createElement("div");
+testBubbleText.classList.add("bubble-content");
+let testBubbleElement = document.createElement("div");
+testBubbleElement.classList.add("bubble", "test-bubble");
+testBubbleElement.appendChild(testBubbleText);
+document.body.appendChild(testBubbleElement);
+
+// Update bubble type when select menu value is changed
 bubbleTypeElement.addEventListener("change", () => {
   updateBubbleType();
 });
 
+// Focus the bubble text input when any part of the bubble is clicked
 bubbleElement.addEventListener("mousedown", (e) => {
   if (e.target == bubbleElement) e.preventDefault(); // prevents unfocus on text when clicked
   bubbleContentElement.focus();
@@ -21,6 +34,13 @@ bubbleElement.addEventListener("mousedown", (e) => {
 bubbleContentElement.addEventListener("paste", (e) => sanitizePaste(e));
 document.addEventListener("drop", (e) => e.preventDefault());
 bubbleContentElement.addEventListener("drop", (e) => sanitizeDrop(e));
+
+// Put the bubble text on the clipboard in MSYT format
+exportBtnElement.addEventListener("mousedown", () => putMsytToClipboard(true));
+document.addEventListener("keydown", (e) => {
+  if (e.code == "Enter" && e.ctrlKey) putMsytToClipboard();
+});
+
 function updateBubbleType() {
   bubbleElement.classList.remove(bubbleType);
   bubbleType = bubbleTypeElement.value;
@@ -37,4 +57,61 @@ function sanitizeDrop(e) {
   e.preventDefault();
   let plaintext = e.dataTransfer.getData("text/plain");
   document.execCommand("insertText", false, plaintext);
+}
+
+function putMsytToClipboard(showShortcutHint) {
+  if (bubbleContentElement.textContent) {
+    let msytExport = '  - text: "';
+    let nodes = Array.from(bubbleContentElement.childNodes);
+    for (let i = 0; i < nodes.length; i++) {
+      let nodeContent = nodes[i].textContent;
+      if (!nodeContent) nodes.splice(i, 1); // gets rid of empty nodes such as <br>
+    }
+    for (let i = 0; i < nodes.length; i++) {
+      let nodeContent = breakTextAtWrap(nodes[i].textContent);
+      if (nodeContent) {
+        msytExport += nodeContent;
+        if (i != nodes.length - 1) msytExport += "\\n";
+      }
+    }
+    msytExport += '"';
+    navigator.clipboard.writeText(msytExport).then(
+      () => {
+        let alertMsg = "The MSYT output has been copied to your clipboard. You can now open your Bootup_LANG.pack file in Wildbits, open an MSBT file, and paste your MSYT output under the `content` field of any text entry.";
+        if (showShortcutHint) alertMsg += "\n\nProtip: you can use Ctrl + Enter as a keyboard shortcut for this feature.";
+        window.alert(alertMsg);
+      },
+      () => {
+        window.alert("Couldn't access the clipboard.");
+      }
+    );
+  } else {
+    window.alert("There's nothing to copy. Try typing some text in the dialogue bubble.");
+  }
+}
+
+function breakTextAtWrap(text) {
+  let fontSize = parseInt(window.getComputedStyle(testBubbleText).getPropertyValue("font-size"));
+  let words = text.split(" ");
+  let testString;
+  let outputString;
+  let output = [];
+  while (words.length > 0) {
+    testString = "";
+    testBubbleText.textContent = "";
+    while (testBubbleText.offsetHeight <= fontSize * 1.25) {
+      outputString = testString;
+      if (outputString) words.splice(0, 1);
+      if (words.length == 0) break;
+      testString += words[0];
+      if (words.length > 1) testString += " ";
+      testBubbleText.textContent = testString;
+    }
+    output += outputString;
+    if (words.length > 0) {
+      output = output.slice(0, -1);
+      output += "\\n";
+    }
+  }
+  return output;
 }
